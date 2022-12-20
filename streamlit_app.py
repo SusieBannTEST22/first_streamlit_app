31c23203a5aa6080880b09872e6d5fbabfc88e69
SB <121113470+SusieBannTEST22@users.noreply.github.com>
2022-12-20 15:53:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 import streamlit
 import pandas
 import requests
@@ -6,24 +5,20 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My parents New Healthy Diner')
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
+
+my_fruit_list = my_fruit_list.set_index('Fruit')# Let's put a pick list here so they can pick the fruit they want to include 
+
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+fruits_to_show = my_fruit_list.loc[fruits_selected]# Display the table on the page.streamlit.dataframe(fruits_to_show)
 
 
 #create the repeatble code block (called a function)
@@ -41,20 +36,11 @@ try:
       streamlit.error("Please select a fruit to get information.")
   else:
       back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
-      
+      streamlit.dataframe(back_from_function)     
 except URLError as e:
   streamlit.error()
-#streamlit.write('The user entered ', fruit_choice)
 
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
